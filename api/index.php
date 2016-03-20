fbfad43a4c7368a1086197932deecd57672b9f78
Ronnie Nyaga <ronnienyaga@gmail.com>
2016-03-20 21:43:41 +0300

    Sign up API > Google recaptcha > Integration of signup submodules

--- api/index.php
+++ api/index.php
@@ -1,13 +1,16 @@
 <?php
 	include '../classes/inc.php';
 	require 'Slim/Slim.php';
-	$marray=array();
 
+	$marray = array();
 	global $app;
 	$app= new Slim();
-	/*******************************************************************/ //Login And logout
+	/*******************************************************************/ //Login, Signup And Logout
 	$app->post('/users/login','login');
+	$app->post('/users/signup','signup');
 	$app->post('/users/logout','logout');
+
+	/******************************************************************/ //
 
 	//trait_exists
 	$app->get('/test','test');
@@ -21,47 +24,95 @@
 		$request = $app->request();
 		$reqdata = json_decode($request->getBody());
 
-		$ss = "SELECT id,names,state,cover FROM users WHERE email=? AND password=?";
-		$stmt = $pdo->prepare($ss);
-		$stmt->execute(array($reqdata->email,md5($reqdata->password)));
-		$row = $stmt->fetch(PDO::FETCH_ASSOC);
-		if ($stmt->rowCount() > 0){
-			if ($row['state'] == 0){
-				$marray['response'] = "deactivated";
-				echo json_encode($marray);
-				return false;
+		try{
+			$ss = "SELECT id,names,state,cover FROM users WHERE email=? AND password=?";
+			$stmt = $pdo->prepare($ss);
+			$stmt->execute(array($reqdata->email,md5($reqdata->password)));
+			$row = $stmt->fetch(PDO::FETCH_ASSOC);
+			if ($stmt->rowCount() > 0){
+				if ($row['state'] == 0){
+					$marray['response'] = "deactivated";
+					echo json_encode($marray);
+					return false;
+				}else{
+					$marray['response']	= "success";
+					$marray['username']	= $row['names'];
+					$marray['user_id'] = $row['id'];
+					$marray['cover'] = $row['cover'];
+
+					$_SESSION['user_id'] = NULL;
+					$_SESSION['username'] = NULL;
+					$_SESSION['cover'] = NULL;
+
+					unset($_SESSION['user_id']);
+					unset($_SESSION['username']);
+					unset($_SESSION['cover']);
+
+					$_SESSION['user_id'] = $row['id'];
+					$_SESSION['username']	= $row['names'];
+					$_SESSION['cover'] = $row['cover'];
+
+					echo json_encode($marray);
+					return false;
+				}
 			}else{
-				$marray['response']	= "success";
-				$marray['username']	= $row['names'];
-				$marray['user_id'] = $row['id'];
-				$marray['cover'] = $row['cover'];
-
-				$_SESSION['user_id'] = NULL;
-				$_SESSION['username'] = NULL;
-				$_SESSION['cover'] = NULL;
-
-				unset($_SESSION['user_id']);
-				unset($_SESSION['username']);
-				unset($_SESSION['cover']);
-
-				$_SESSION['user_id'] = $row['id'];
-				$_SESSION['username']	= $row['names'];
-				$_SESSION['cover'] = $row['cover'];
-
+				$marray['response']="fail";
 				echo json_encode($marray);
 				return false;
 			}
-		}else{
-			$marray['response']="fail";
-			echo json_encode($marray);
-			return false;
+		}catch(PDOException $e){
+			$marray['messsage'] = 'error';
+			$marray['error'] = 'Code 9001: '.$e->getMessage();
 		}
 		return true;
 	}; //Login
 
+	function signup(){
+			$pdo = getConnection();
+			$marray = array();
+			global $app;
+			$request = $app->request();
+			$reqdata = json_decode($request->getBody());
+			try{
+				//Check if account exists
+				$ss = "SELECT id FROM users WHERE email=?";
+				$stmt = $pdo->prepare($ss);
+				$stmt->execute(array($reqdata->email));
+				$row = $stmt->fetch(PDO::FETCH_ASSOC);
+				if($stmt->rowCount() > 0){
+					$marray['response'] = 'exists';
+				}else{
+					$ss = "INSERT INTO users (names,email,phone,company,cover,password) VALUES (?,?,?,?,?,?)";
+					$stmt = $pdo->prepare($ss);
+					$stmt->execute(array($reqdata->names,$reqdata->email,$reqdata->phone,$reqdata->company,$reqdata->cover,md5($reqdata->password)));
+					$userid = $pdo->lastInsertId();
+
+					$marray['response'] = 'success';
+					$marray['username']	= $reqdata->names;
+					$marray['user_id'] = $userid;
+					$marray['cover'] = $reqdata->cover;
+
+					$_SESSION['user_id'] = NULL;
+					$_SESSION['username'] = NULL;
+					$_SESSION['cover'] = NULL;
+
+					unset($_SESSION['user_id']);
+					unset($_SESSION['username']);
+					unset($_SESSION['cover']);
+
+					$_SESSION['user_id'] = $userid;
+					$_SESSION['username']	= $reqdata->names;
+					$_SESSION['cover'] = $reqdata->cover;
+				}
+			}catch(PDOException $e){
+				$marray['response'] = 'error';
+				$marray['error'] = 'Code 9002: '.$e->getMessage();
+			}
+			echo json_encode($marray);
+			return true;
+	}
+
 	function logout(){
-		$pdo = getConnection();
-		global $app;
 		$_SESSION['user_id'] = NULL;
 		$_SESSION['username'] = NULL;
 		$_SESSION['cover'] = NULL;
--- asset/js/signup.js
+++ asset/js/signup.js
@@ -17,7 +17,7 @@ $(document).ready(function(e){
       $('#cover').val(data.name);
       $('.mycover').html('<img class="img-responsive thumbnail" style="width:300px;" src="documents/'+data.name+'">');
       $('.ajax-upload-dragdrop').hide();
-      
+
     },
     onError: function(files,status,errMsg,pd){
       attachment="Error_on_upload"; pd.done.show();
@@ -27,4 +27,42 @@ $(document).ready(function(e){
       });
     },
   });// Upload File
+
+  $('form').submit(function(event){
+    event.preventDefault();
+    event.stopPropagation();
+    var myform = $(this);
+
+    //Google Recaptcha. Check it is checked before submitting
+    if(myform.find('.g-recaptch-response').val() == ''){
+      $('.robot').removeClass('hidden');
+      return false;
+    }else{
+      $('.sk-wave').removeClass('hidden');
+      submitform( myform, function(data){
+        $('.sk-wave').addClass('hidden');
+        if(data.response == "success"){
+          myform[0].reset();
+          $('.alert-success').removeClass('hidden');
+          $('.alert-danger').addClass('hidden');
+          $('.alert-warning').addClass('hidden');
+          setCookie('theuser',data.username,1);
+          setCookie('cover',data.avatar,1);
+          setCookie('user_id',data.user_id,1);
+          window.location = 'dashboard.html';
+        }if(data.response == "exists"){
+          $('.alert-success').addClass('hidden');
+          $('.alert-danger').addClass('hidden');
+          $('.alert-warning').removeClass('hidden');
+          return false;
+        }else if(data.response == "error"){
+          $('.alert-success').addClass('hidden');
+          $('.alert-danger').removeClass('hidden');
+          $('.alert-warning').addClass('hidden');
+          console.log(data.error);
+          return false;
+        }
+      });
+    }
+  }) //! Form Submit
 })
