14f42ec522a88fbf3aec79981bfcd6d9fbe04453
Ronnie Nyaga <ronnienyaga@gmail.com>
2016-03-20 04:16:31 +0300

    Javascript logic for login complete

--- asset/js/login.js
+++ asset/js/login.js
@@ -1,32 +1,35 @@
 $(document).ready(function(e){
   setCookie('theuser','GUEST',1); delete_cookie('theuser');
   setCookie('user_id','0',1); delete_cookie('user_id');
+  setCookie('avatar','0',1); delete_cookie('avatar');
 
   $('form').submit(function (event) {
     event.preventDefault();
     event.stopPropagation();
-    thisform = $(this)
-    $(this).children('.mjeerror').hide();$(this).children('.mjenotactive').hide();$(this).children('.mjesuccess').hide();
-    var fdata = $(this).serializeArray();
-    fdata.push({name:'token',value:newtoken('LOGIN')});
-    $.post('api/',fdata,function(data){
-      //console.log("debug");
-      if(data.response=="success"){
-        console.log("success");
-        thisform.children('.mjesuccess').show();
-        var date = new Date();
-        var minutes = 30;
-        date.setTime(date.getTime() + (minutes * 60 * 1000));
-        //$.cookie("cookie", "value", { expires: date });
-        setCookie('theuser',data.theuser, { expires:date });
-        setCookie('user_id',data.user_id,{ expires:date });
-        setCookie('user_role',data.user_role,{ expires:date });
-        window.location='dashboard.html';
-      }else{
-        thisform.children('.mjefail').show();
-        console.log("fail");
+    var fdata = {'email':$('#email').val(),'password':$('#password').val()};
+    obj = JSON.stringify(fdata);
+    var dataR = ajaxAPICall('POST',obj,'api/users/login','application/json');
+    dataR.success(function(data){
+      if(data.response == 'success'){
+        $('.alert-success').show();
+        $('.alert-danger').hide();
+        $('.alert-warning').hide();
+        setCookie('theuser',data.username,1);
+        setCookie('avatar',data.avatar,1);
+        setCookie('user_id',data.id,1);
+        window.location = 'dashboard.html';
       }
-    },'json')
-    return false;
+      if(data.response == 'fail'){
+        $('.alert-success').hide();
+        $('.alert-danger').show();
+        $('.alert-warning').hide();
+      }
+      if(data.response == 'error'){
+        $('.alert-success').hide();
+        $('.alert-danger').hide();
+        $('.alert-warning').show();
+      }
+      return false;
+    })
   });
 })
